f240387e154776213a81b37fd18adb0673b672b9
toussel <benjamin.hitter@gmail.com>
2023-04-06 21:24:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+//reggel
+//var morningcompliments =["Jó Reggelt!", "Legyen Jó Napod!" , "Bottyán János Technikum 2023"]
+
+//delben
+//var afternooncompliments =["Szia!","Hello!", "Bottyán János Technikum 2023"]
+
+//este
+//var nightcompliments =["Szép Álmokat!","Álmodj Szépeket!", "Bottyán János Technikum 2023"]
+
+document.querySelector("#myButton").onclick = function(){
+
+    const h2 = document.querySelector("#myH2");
+    h2.innerText = "Bye World!";
+
+}
