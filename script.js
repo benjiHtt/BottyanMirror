8ca92caa8b19929d9d5bf6f3ea4e84bfbe79367a
toussel <benjamin.hitter@gmail.com>
2023-06-04 14:44:32 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -7,6 +7,27 @@
 
 //este
 //var nightcompliments =["Szép Álmokat!","Álmodj Szépeket!", "Bottyán János Technikum 2023"]
+setInterval(setClock, 1000)
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock() {
+  const currentDate = new Date()
+  const secondsRatio = currentDate.getSeconds() / 60
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+  setRotation(secondHand, secondsRatio)
+  setRotation(minuteHand, minutesRatio)
+  setRotation(hourHand, hoursRatio)
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
 
 document.querySelector("#myButton").onclick = function(){
 
